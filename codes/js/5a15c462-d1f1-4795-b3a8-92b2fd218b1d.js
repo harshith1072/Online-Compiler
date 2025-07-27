90c3da075b7944fe53bf421819022b7200054954
harshithmateti <harshithmateti2005@gmail.com>
2025-07-27 18:28:08 +0530

    Removed codes folder from tracking

--- codes/js/5a15c462-d1f1-4795-b3a8-92b2fd218b1d.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let input = fs.readFileSync(0, 'utf-8');
-let [a, b] = input.trim().split(' ').map(Number);
-console.log(a + b);
--- codes/js/f614260b-66b7-4ca1-a844-088ddfba7ad2.js
+++
@@ -1,4 +0,0 @@
-process.stdin.on('data', d => {
-  let [a, b] = d.toString().split(' ').map(Number);
-  console.log(a + b);
-});
